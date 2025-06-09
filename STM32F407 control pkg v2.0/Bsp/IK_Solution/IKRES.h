2717e0f7ccbec87b8f0a0e248d3a80ca7887d6bc
Li-Chaoran <chaoranfeifan666@gmail.com>
2025-06-09 11:36:16 +0800

    update v2.0 hardware library

--- STM32F407 control pkg v2.0/Bsp/IK_Solution/IKRES.h
+++
@@ -1,9 +0,0 @@
-#ifndef IKRES_H
-#define IKRES_H
-
-#include <stdint.h>
-
-extern const int16_t ik_solution_array[3][3721];
-
-#endif // IKRES_H
-
--- STM32F407 control pkg v2.0/MDK-ARM/Underwater vector propeller control pkg/ikres.d
+++
@@ -1,3 +0,0 @@
-underwater vector propeller control pkg\ikres.o: ..\Bsp\IK_Solution\IKRES.c
-underwater vector propeller control pkg\ikres.o: ..\Bsp\IK_Solution\IKRES.h
-underwater vector propeller control pkg\ikres.o: D:\KEIL5\core\ARM\ARMCC\Bin\..\include\stdint.h
